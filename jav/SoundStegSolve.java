e284e0b4cd122d30465da1250abe74b541a8de1b
ichenadamczyk20 <ichenadamczyk20@stuy.edu>
2022-05-20 15:01:37 -0400

    sudo code

--- jav/SoundStegSolve.java
+++ jav/SoundStegSolve.java
@@ -1,24 +1,5 @@
 // read wav file
 
-// if the first 4 bytes of the sound are 1000, then we make the last byte the data
+// if the first 4 bytes of the sound are 1000, then we make the last byte the data and the second-last byte 0
 
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
-
-public class SoundStegSolve {
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("filename.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
-  }
-}
+// at end of data , make the second-last byte 1 always
