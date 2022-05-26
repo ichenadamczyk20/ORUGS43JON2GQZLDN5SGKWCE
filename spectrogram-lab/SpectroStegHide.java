e4c74c8da17c906bcf66622972551703336cc550
Alex Cho <acho20@stuy.edu>
2022-05-26 15:07:10 -0400

    increased width of the image

--- spectrogram-lab/SpectroStegHide.java
+++ spectrogram-lab/SpectroStegHide.java
@@ -29,15 +29,9 @@ public class SpectroStegHide {
 
     public static void main(String[] args){
         try{
-            System.out.println("Usage: java SpectroStegHide <input audio> <file to hide> <output audio>");
-
-            String inputSound = args[0];
-            String inputFile = args[1];
-            String outputSound = args[2];
-
-
-            File iFile = new File(inputSound);
-            byte[] beginner = Files.readAllBytes(iFile.toPath());
+            System.out.println("Usage: java SpectroStegHide<file to hide> <output audio>");
+            String inputFile = args[0];
+            String outputSound = args[1];
 
             File file = new File(outputSound);
             FileOutputStream out = new FileOutputStream(file);
@@ -45,7 +39,7 @@ public class SpectroStegHide {
             BufferedImage img = ImageIO.read(new File(inputFile));
 
             byte[] header = new byte[]{-46, -55, -58, -58, 44, -40, -127, -128, -41, -63, -42, -59, -26, -19, -12, -96, -112, -128, -128, -128, -127, -128, -127, -128, -60, 44, -128, -128, 8, -40, -127, -128, -126, -128, -112, -128, -28, -31, -12, -31, 8, -40, -127, -128};
-            byte[] conclusion = new byte[beginner.length];
+            byte[] conclusion = new byte[img.getWidth() * 40 + 44];
             for(int i = 0; i < 44; i++){
               conclusion[i] = header[i];
             }
@@ -56,8 +50,8 @@ public class SpectroStegHide {
             int t = 1; // HARD CODED to ONE SECOND o KAY?
             int lowFreq = 200;
             int highFreq = 8000;
-            for (int x = 0; x < width; x++) {
-                double ty = t / ((double) x);
+            for (int x = 0; x < 40 * width; x++) {
+                double ty = t / ((double) x * 40);
                 double val = 0;
                 for (int y = 0; y < height; y++) {
                     int red = img.getRGB(x, y) / (256 * 256);
@@ -68,7 +62,9 @@ public class SpectroStegHide {
                     val += grey * Math.sin(ty * freq * 2 * 3.1415926);
                 }
                 byte theActual = (byte) ((int) (64 * val));
-                conclusion[x + 44] = theActual;
+                for(int i = 0; i < 40; i++){
+                  conclusion[x + 44 + i] = theActual;
+                }
             }
             out.write(conclusion);
             out.close();
