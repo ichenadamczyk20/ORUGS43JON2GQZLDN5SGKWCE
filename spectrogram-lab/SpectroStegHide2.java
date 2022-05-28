d1dd34d53fe38b0c632940e01cd3346dc34284a2
ichenadamczyk20 <ichenadamczyk20@stuy.edu>
2022-05-28 19:13:07 -0400

    cri cri

--- spectrogram-lab/SpectroStegHide2.java
+++ spectrogram-lab/SpectroStegHide2.java
@@ -34,15 +34,15 @@ public class SpectroStegHide {
             String outputSound = args[1];
 
             File file = new File(outputSound);
-            FileOutputStream out = new FileOutputStream(file);
 
             BufferedImage img = ImageIO.read(new File(inputFile));
 
             byte[] header = new byte[]{82, 73, 70, 70, 36, -90, 14, 0, 87, 65, 86, 69, 102, 109, 116, 32, 16, 0, 0, 0, 1, 0, 1, 0, -128, -69, 0, 0, 0, 119, 1, 0, 2, 0, 16, 0, 100, 97, 116, 97, 0, -90, 14, 0};
             byte[] conclusion = new byte[960000 + 44];
             for(int i = 0; i < 44; i++){
-              conclusion[i] = header[i];
+                conclusion[i] = header[i];
             }
+            //out.write(header);
 
             int width = img.getWidth();
             int height = img.getHeight();
@@ -53,40 +53,53 @@ public class SpectroStegHide {
             double factor = (highFreq - lowFreq) / ((double) height);
             int samplesPerPx = (int) ((int) 480000 / (int) img.getWidth());
             int x = -1;
+            double average = 0;
+            double ampFactor = 20;
+            int start = 0;
             for (int s = 0; s < 480000; s++) {
                 double ty = s * t / ((double) 480000 );
+                double ty2pi = ty * 2 * 3.1415926;
                 double val = 0;
-                if ((s % samplesPerPx) == 0 && (x < width - 1))
+                if ((s % samplesPerPx) == 0 && (x < width - 1)) {
                     x++;
-                for (int y = 0; y < height; y++) {
-                    int color = img.getRGB(x,y);
-                    int blue = color & 0xff;
-                    int green = (color & 0xff00) >> 8;
-                    int red = (color & 0xff0000) >> 16;
-                    double grey = (red + green + blue) / ((double) 768);
-                    double freq = ((height - y) * factor) + lowFreq;
-                    val += grey * Math.sin(ty * freq * 2 * 3.1415926);
+                    start += 1;
+                    start %= 3;
                 }
-
-                /*if (((s % samplesPerPx) > (samplesPerPx * 0.95)) && (x < width - 1)) {
-                    for (int y = 0; y < height; y++) {
-                        int color = (int) ((0.5 * img.getRGB(x,y)) + (0.5 * img.getRGB(x + 1, y)));
-                        int blue = color & 0xff;
-                        int green = (color & 0xff00) >> 8;
-                        int red = (color & 0xff0000) >> 16;
-                        double grey = (red + green + blue) / ((double) 768);
-                        double freq = ((height - y) * factor) + lowFreq;
-                        val += grey * Math.sin(ty * freq * 2 * 3.1415926);
+                for (int y = start; y < height; y+=1) {
+                    int color = img.getRGB(x,y);
+                    double blue = color & 0xff;
+                    double green = (color & 0xff00) >> 8;
+                    double red = (color & 0xff0000) >> 16;
+                    double grey = (0.2126 * red + 0.7152 * green + 0.0722 * blue) / ((double) 768);
+                    for (double z = 0; z < 1; z += 0.01) {
+                        double freq = ((height - y - z) * factor) + lowFreq;
+                        val += grey * Math.sin(ty2pi * freq);
                     }
-                }*/
+                    //val *= grey;
+                }
                 byte theActual;
-                if (val > 0) theActual = (byte) ((int) (20 * Math.pow(val, 0.3)));
-                else theActual = (byte) ((int) (-20 * Math.pow(-1 * val, 0.3)));
+                if (val > 0) theActual = (byte) ((int) (ampFactor * Math.pow(val, 0.5)));
+                else theActual = (byte) ((int) (-1 * ampFactor * Math.pow(-1 * val, 0.5)));
+                //out.write(0);
+                //out.write(theActual);
                 conclusion[s * 2 + 44] = 0;
                 conclusion[s * 2 + 1 + 44] = theActual;
+                average += Math.abs((int) theActual);
+//                if (s % 50 == 0) {
+//                    start += 1;
+//                    start %= 10;
+//                }
+                if (s % 1000 == 0 && s != 0) {
+                    FileOutputStream out = new FileOutputStream(file);
+                    out.write(conclusion);
+                    out.close();
+                    System.out.println(s);
+
+                    average /= 100;
+                    //ampFactor = 32 / average;
+                    average = 0;
+                }
             }
-            out.write(conclusion);
-            out.close();
         }catch(FileNotFoundException e){
             System.out.println("ERROR: File not found");
             e.printStackTrace();
